bbe4babef7fa033f04ab54059946e6127776a521
sunabove <sunabove@nate.com>
2024-11-07 08:15:43 +0900

    **

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.myapp;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
